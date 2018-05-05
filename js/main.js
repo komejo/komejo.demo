400bf14c1f1b22db06cfcdf29285f8d5216c48b1
Joe Komenda <komejo@gmail.com>
2018-05-05 10:47:42 -0700

    Basic page setup.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**
+ * File main.js.
+ *
+ * Everything is wrapped in an Immediately-Invoked
+ * Function Expression (IIFE) for security/scope.
+ *
+ * See http://benalman.com/news/2010/11/immediately-invoked-function-expression/
+ */
+( function() {
+
+
+
+
+
+} )();
