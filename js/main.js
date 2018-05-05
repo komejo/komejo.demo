e80ebed3a34c7d19bb9ede5c366cdabd18f1a871
Joe Komenda <komejo@gmail.com>
2018-05-05 11:35:38 -0700

    Get REST api up and running.

--- js/main.js
+++ js/main.js
@@ -8,8 +8,35 @@
  */
 ( function() {
 
+  // Get all the blog posts.
+  const apiUrl = `https://voorhoede-colibri-api-zhmoaomjvy.now.sh/api/v1`
 
+  function restRequest(query) {
+    return fetch(`${apiUrl}${query}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => response.json())
+  }
 
+  // Get the data back in a useable/cacheable state.
+  var blogInfo = new Promise(function(resolve, reject) {
+    resolve(restRequest(`/blog`));
+  });
+
+  blogInfo.then(function(blog) {
+    // console.log( blog )
+  });
+
+  var allPosts = new Promise(function(resolve, reject) {
+    resolve(restRequest(`/posts`));
+  });
+
+  allPosts.then(function(posts) {
+    // console.log( posts )
+  });
 
 
 } )();
