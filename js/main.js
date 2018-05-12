a815f5af00e445b5d3087b100f0d7ce645b39e99
Joe Komenda <komejo@gmail.com>
2018-05-12 12:09:06 -0700

    Move search function out of the EventListener, add default search, fix non-unique IDs.

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@
       }
     })
     .then(response => response.json())
-  }
+  };
 
   // Get the data back in a useable state.
   let blogInfo = new Promise(function(resolve, reject) {
@@ -32,86 +32,110 @@
         subtitle = document.querySelectorAll('.subtitle'),
         description = document.querySelectorAll('.description');
     title[0].prepend(blog.title + ' ');
-    subtitle[0].innerHTML += blog.subtitle;
-    description[0].innerHTML += blog.description;
+    subtitle[0].innerHTML = blog.subtitle;
+    // description[0].innerHTML = blog.description;
   });
-
 
   // Now get all the posts and put them in an object.
   let allPosts = new Promise(function(resolve, reject) {
     resolve(restRequest(`/posts`));
   });
 
-  // Add vanilla event listner to the search form.
+  // Primary search function.
+  function searchResults(formData = '', searchByWeight) {
+
+    // Default search;
+    let searchTerm = '';
+    if (formData.length != 0) {
+      searchTerm = formData.get('search-box').toLowerCase()
+    };
+
+    // Apply toLowerCase to the formData argument.
+    let searchResults = [],
+        resultCount = document.querySelectorAll('.search-results-count'),
+        resultPreview = document.querySelectorAll('.search-results');
+
+    // Search the posts for matching strings.
+    allPosts.then(function(posts) {
+      // Debug - so we can see what's available.
+      // console.log( posts[0] )
+      let resultText = "";
+
+      for (let i = 0; i < posts.length; i++){
+        // Apply toLowerCase to blogPosts.
+        let blogPosts = posts[i].body.toLowerCase();
+
+        // Create an object that has the post id and the count of terms.
+        if (blogPosts.indexOf(searchTerm) >= 0){
+          let result = {};
+          result.post = i;
+          result.weight = (blogPosts.split(searchTerm).length - 1);
+          searchResults.push(result);
+        }
+      }
+
+      // Sort by weight.
+      if ( searchByWeight ) {
+        searchResults.sort(function(a, b){
+          return b.weight - a.weight;
+        });
+        resultText = 'sorted by relevance.';
+      } else {
+        resultText = 'sorted by most recent.';
+      };
+
+      // Update results count.
+      let searchCount = searchResults.length,
+          resultPlural = ' results, ';
+
+      // 1 'results' makes me crazy.
+      // Using logic operator for fun/compactness.
+      searchCount == 0 && (resultText = 'perhaps try again?');
+      searchCount == 1 && (resultPlural = ' result, ');
+
+      // Show 3 blog posts by default.
+      if (formData.length == 0) {
+        resultCount[0].innerHTML = 'Showing most recent three out of ' + searchCount + ' blog posts.'
+        searchCount = 3;
+      } else {
+        resultCount[0].innerHTML = searchCount + resultPlural + resultText;
+      };
+
+      // Get all the previews and post them as links.
+      let postPreview = '';
+      for(let j = 0; j < searchCount; j++){
+        let key = searchResults[j].post;
+
+        postPreview += '<li>';
+        postPreview +=  '<h4><a href="https://www.voorhoede.nl/en/blog/' + posts[key].slug + '/">' + posts[key].title + '</a></h4>';
+        // Check that there's a preview!
+        posts[key].teaser != null && (postPreview += posts[key].teaser);
+        postPreview += '</li>';
+      }
+
+      resultPreview[0].innerHTML = postPreview;
+
+    });
+  };
+
+  // Add event listner to the search form.
   window.onload = function () {
-    document.querySelector("#search")
+
+    document.querySelector("#search-form")
     .addEventListener("submit", function(e) {
       e.preventDefault();
 
       // Get the submitted search string.
       let formData = new FormData(e.target),
-          searchByWeight = formData.get('sort-by-weight'),
-          searchTerm = formData.get('search-box').toLowerCase(),
-          searchResults = [],
-          resultCount = document.querySelectorAll('.search-results-count'),
-          resultPreview = document.querySelectorAll('.search-results');
+          searchByWeight = formData.get('sort-by-weight');
 
-      // Search the posts for matching strings.
-      allPosts.then(function(posts) {
-        // Temp - so we can see what's available.
-        // console.log( posts[0] )
-        let resultText = "";
-
-        for (let i = 0; i < posts.length; i++){
-          let str = posts[i].body.toLowerCase();
-
-          // Use toLowerCase for term and posts to find all results.
-          if (str.indexOf(searchTerm) >= 0){
-            let result = {};
-            result.post = i;
-            result.weight = (str.split(searchTerm).length - 1);
-            searchResults.push(result);
-          }
-        }
-
-        if ( searchByWeight ) {
-          searchResults.sort(function(a, b){
-            return b.weight - a.weight;
-          });
-          resultText = 'sorted by relevance.';
-        } else {
-          resultText = 'sorted by most recent.';
-        };
-
-        // Update results count.
-        let searchCount = searchResults.length,
-            resultPlural = ' results, ';
-
-        // 1 'results' makes me crazy.
-        // Using logic operator for fun/compactness.
-        searchCount == 0 && (resultText = 'perhaps try again?');
-        searchCount == 1 && (resultPlural = ' result, ');
-
-        resultCount[0].innerHTML = searchCount + resultPlural + resultText;
-
-        // Get all the previews and post them as links.
-        let postPreview = '';
-        for(let j = 0; j < searchCount; j++){
-          let key = searchResults[j].post;
-
-          postPreview += '<li>';
-          postPreview +=  '<h4><a href="https://www.voorhoede.nl/en/blog/' + posts[key].slug + '/">' + posts[key].title + '</a></h4>';
-          // Check that there's a preview!
-          posts[key].teaser != null && (postPreview +=  posts[key].teaser);
-          postPreview += '</li>';
-        }
-
-        resultPreview[0].innerHTML = postPreview;
-
-      });
+      searchResults(formData, searchByWeight)
 
     }, false);
-  }
+
+    // Default search.
+    searchResults();
+  };
 
 
 } )();
