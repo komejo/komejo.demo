25f031b1fa4dca9f9ef153e41c672e933c8bab8e
Joe Komenda <komejo@gmail.com>
2018-05-13 12:28:26 -0700

    Add clarifying comment

--- js/main.js
+++ js/main.js
@@ -138,6 +138,8 @@
 
     // Page load search.
     searchResults();
+
+    // Fade in loaders after 1 second.
     setTimeout(function() {
       document.querySelector('.loading img').classList.add('fade-in');
       document.querySelector('.loading p').classList.add('fade-in');
